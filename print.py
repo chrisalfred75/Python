8a1b47d7b1f2b3845dcd57f59d328ac72e6e5e8a
chrisalfred75 <116115190+chrisalfred75@users.noreply.github.com>
2022-10-19 02:05:11 +0530

    Python code to PRINT basic commands

---
+++ print.py
@@ -0,0 +1,11 @@
+print ('hello this is chris')
+print ('i am a university student')
+
+
+character_name = 'Tom'
+character_age = '50'
+print('there was one a man named ' + character_name + ', ')
+print('he was ' + character_age + 'years old.')
+print('he really liked the name ' + character_name + ',')
+print('but he did not like being ' + character_age + '.')
+
