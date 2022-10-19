6ba2edf2cdc1af11c7aad1230fae5b274101dfaf
Chris Alfred Ferross <116115190+chrisalfred75@users.noreply.github.com>
2022-10-19 18:27:18 +0530

    Calculator (Add/Sub/Multiply)

---
+++ calculator.py
@@ -0,0 +1,20 @@
+from unittest import result
+
+
+num1 = input('Enter a number: ')
+num2 = input('Enter another number: ')
+result = float(num1) + float(num2)
+
+print(result)
+
+num1 = input('Enter a number: ')
+num2 = input('Enter another number: ')
+result = float(num1) - float(num2)
+
+print(result)
+
+num1 = input('Enter a number: ')
+num2 = input('Enter another number: ')
+result = float(num1) * float(num2)
+
+print(result)
